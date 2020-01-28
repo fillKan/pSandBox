5aa8af5f0b170e04096c257c89e85a46d99eddd9
한대건 <han15050424@gmail.com>
2020-01-28 13:45:19 +0900

    [플레이어 이동 추가] #이동키를 통해서 이동함. #이동하는 방향에 따라서 스프라이트가 Fliping함

---
+++ Object/Player/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 vDir;
+    private SpriteRenderer sprite;
+
+    private void OnEnable()
+    {
+        vDir = transform.position;
+
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+
+        StartCoroutine(CR_update());
+    }
+
+    private IEnumerator CR_update()
+    {
+        while (gameObject.activeSelf)
+        {
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                vDir.x += Input.GetAxis("Horizontal") * Time.deltaTime * 3.5f;
+
+                if (Input.GetAxis("Horizontal") > 0) sprite.flipX = false;
+                if (Input.GetAxis("Horizontal") < 0) sprite.flipX = true;
+
+                transform.position = vDir;
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        yield break;
+    }
+}
