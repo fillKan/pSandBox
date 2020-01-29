2da15c66f63becd622318d6e8ae2e98c70508bef
한대건 <han15050424@gmail.com>
2020-01-29 10:09:19 +0900

    [나무 추가] #참나무, 자작나무, 그리고 나무들의 부모가 되는 Tree클래스 추가. #스페이스 바를 통해 나무와 상호작용 하고, 충분한 상호작용이 이루어지면 나무를 베어뜨림. #나무들의 부모가 되는 Tree클래스를 추가하기는 했지만, 아직 상속은 이루어지지 않은 상태. #현재 나무 상호작용은 참나무만 가능함.
    [패치]
    #참나무와 자작나무의 리소스를 각각의 폴더로 분리
    #플레이어에게 collider와 rigidbody컴포넌트 추가.
    #플레이어와 나무의 상호작용이 원활하지 않다는 이유로, 플레이어 rigidbody컴포넌트의 Collision Detection(충돌을 확인하는 주기)속성을 Continuous(더 많은 CPU소모)로 변경.
    #플레이어의 rigidbody컴포넌트의 Sleeping Mode속성을 Start Asleep으로 변경. 이유는 충돌이 별로 없을것 같아서리...

---
+++ Object/Tree/Tree.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+
+    protected bool doingChopTree = false;
+    protected float fDurability;
+
+    #region 변수 설명 :
+    /*
+    *  sprite        : 나무의 SpriteRenderer컴포넌트를 담는 변수
+    *  doingChopTree : 나무를 베고있는 중인지를 담는 변수
+    *  fDurability   : 나무의 내구도를 담는 변수
+    */
+    #endregion
+
+
+    private void OnEnable()
+    {
+        fDurability = 10;
+
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && !doingChopTree)
+            {
+                StartCoroutine(CR_chopTree());
+            }
+        }
+    }
+
+    protected IEnumerator CR_chopTree()
+    {
+        doingChopTree = true;
+        fDurability  -= 3;
+
+        #region 변수 설명
+
+        float fTime = 0.4f;
+        Vector2 vInitPos = transform.position;
+
+        /*
+         * 오브젝트 흔들기의 지속시간 설정, 
+         * 흔들기 이전의 오브젝트의 위치 저장, 
+         * 나무의 내구도 감소
+         */
+        #endregion
+
+
+        // 0.4초 동안 오브젝트 흔들기
+        while (fTime > 0)
+        {
+            fTime -= Time.deltaTime;         
+
+            transform.position = ((Vector2)Random.insideUnitSphere * 0.1f) + vInitPos;
+            yield return new WaitForFixedUpdate();
+        }
+        // 위치를 다시 처음 위치로
+        transform.position = vInitPos;
+
+        // 나무의 내구도가 0 이하라면, 나무를 쓰러뜨리는 코루틴을 실행시키고,
+        // 실행시킨 코루틴이 종료되면 오브젝트를 비활성화한 뒤 코루틴을 종료시킨다. 
+        if (fDurability <= 0)
+        {
+            yield return StartCoroutine(CR_chopDownTree());
+
+            gameObject.SetActive(false);
+
+            yield break;
+        }
+        doingChopTree = false;        
+    }
+
+    protected IEnumerator CR_chopDownTree()
+    {
+        float alpha = 1;
+
+        // Fade . . .
+        while (sprite.color.a > 0)
+        {
+            alpha -= 0.02f;
+            sprite.color = new Color(1, 1, 1, alpha);
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        yield break;
+    }
+}
