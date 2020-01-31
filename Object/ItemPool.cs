b958d3f67b308bda6cf22f74d1bf8489d98f4abe
한대건 <han15050424@gmail.com>
2020-01-31 11:48:08 +0900

    [아이템 제작 중] #Item들을 관할하는 ItemPool스크립트 제작 중

---
+++ Object/ItemPool.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPool : Singleton<ItemPool>
+{
+    private Dictionary<int, Item> m_items = new Dictionary<int, Item>();
+
+    public Item[] itemList;
+
+    private void Awake()
+    {
+        for(int i = 0; i < itemList.Length; i++)
+        {
+            m_items.Add(itemList[i].itemCode, itemList[i]);
+        }
+    }
+
+    public Item ItemInstantiate(int itemCode)
+    {
+        return null;
+    }
+}
