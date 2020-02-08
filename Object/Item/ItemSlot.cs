f826f8a15eb1a731bcf6ed090dae161f6c457feb
한대건 <han15050424@gmail.com>
2020-02-08 11:10:26 +0900

    [아이템 컨테이너 제작 중] #한 종류의 아이템을 저장할 수 있는 ItemSlot 클래스 추가. #인벤토리와 같은 아이템 컨테이너는 ItemSlot라는 개체로 구성됨.
    [패치]
    #아이템들의 스프라이트 최대 크기 수정

---
+++ Object/Item/ItemSlot.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+    private Stack<Item> _itemContainer = new Stack<Item>();
+    
+    public int ItemCount
+    {
+        get { return _itemContainer.Count; }
+    }
+    public Item ContainItem
+    {
+        get { return _itemContainer.Peek(); }
+    }
+
+    public Text text;
+
+    public void AddItem(params Item[] items)
+    {
+        int i = 0;
+
+        if (ItemCount == 0)
+        {
+            _itemContainer.Push(items[i++]);
+        }
+
+        for (; i < items.Length; i++)
+        {
+            if (_itemContainer.Peek() == items[i])
+            {
+                _itemContainer.Push(items[i]);
+            }
+            else Debug.LogWarning("적합하지 않은 아이템은 추가할 수 없습니다.");
+        }
+        UpdateItemCount();
+    }
+
+    public void AddItem(Item item)
+    {
+        if (_itemContainer.Count == 0)
+        {
+            _itemContainer.Push(item);
+        }
+
+        else if (item == _itemContainer.Peek())
+        {
+            _itemContainer.Push(item);          
+        }
+        UpdateItemCount();
+    }
+
+    public void UpdateItemCount()
+    {
+        text.text = ItemCount.ToString();
+    }
+}
