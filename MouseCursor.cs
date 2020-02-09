4b9f0bddbe444d86aea61a8ed8d20f9be885e7c9
한대건 <han15050424@gmail.com>
2020-02-09 11:39:25 +0900

    [마우스 상호작용 제작 중] #상호작용 방식을 플레이어가 직접 이동하기 보다, 마우스 클릭을 통한 상호작용으로 바꿀 것. #현재 마우스의 위치가 변할 때마다, 광선을 투사한다. 투사한 광선은 플레이어와 상호작용 하는 레이어하고만 충돌한다. #마우스의 움직임에 유의미한 변화가 있을 경우에만 광선을 투사하도록 바꿀 계획.
    #참나무 오브젝트는 두 개의 콜라이더를 가짐(더 넓은 상호작용 범위).
    #아이템 슬롯 리소스 추가.

---
+++ MouseCursor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : Singleton<MouseCursor>
+{
+    private Vector2 vPrevPos;
+    private Vector2 vCurrPos;
+
+    public Camera MainCamera;
+
+    private void Start()
+    {
+        StartCoroutine(CR_update());
+    }
+
+    private IEnumerator CR_update()
+    {
+        int layer = 1 << LayerMask.NameToLayer("Player Interaction");
+
+        vPrevPos.x = (long)(Input.mousePosition.x);
+        vPrevPos.y = (long)(Input.mousePosition.y);
+        vCurrPos = vPrevPos;
+
+        while(true)
+        {
+            vCurrPos.x = (long)(Input.mousePosition.x);
+            vCurrPos.y = (long)(Input.mousePosition.y);
+
+            if (vCurrPos != vPrevPos)
+            {
+                vPrevPos = vCurrPos;
+                Vector2 vRayPos = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+                RaycastHit2D hit2D = Physics2D.Raycast(vRayPos, Vector2.zero, 10, layer);
+
+                if (hit2D)
+                {
+                    Debug.Log(hit2D.point);
+                }
+            }
+            yield return null;
+        }
+    }
+}
