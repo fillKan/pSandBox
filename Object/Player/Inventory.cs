4d36c40ddb1bfdf41b3339c65af445879e8e2f7e
한대건 <han15050424@gmail.com>
2020-02-14 19:20:19 +0900

    [인벤토리에 아이템을 추가하는 방식 개선] #인벤토리에 동일한 아이템코드의 아이템이 존재하는지 먼저 검색한다. #검색하던 중, 가장 처음에 발견한 빈 인덱스의 번호를 emptySlotIndex에 저장한다. #상기 검색 과정이 끝났음에도 아이템이 제자리를 찾지 못했다면, emptySlotIndex번째 인덱스에 아이템을 저장한다. #만약 동일한 아이템이 보관된 슬롯도, 빈 슬롯도 검색하지 못했다면 에러 로그를 띄운다.

--- Object/Player/Inventory.cs
+++ Object/Player/Inventory.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     public ItemSlot[] itemSlots;
+    private sbyte empty = -1;
 
     public void AddItemInventory(Item item)
     {
+        int emptySlotIndex = empty;
+
         item.gameObject.SetActive(false);
 
         for(int i = 0; i < itemSlots.Length; i++)
         {
             if (itemSlots[i].ContainItem == null)
             {
+                if (emptySlotIndex.Equals(empty))
+                {
+                    emptySlotIndex = i;
+                }
                 continue;
             }
 
@@ -23,14 +30,12 @@ public class Inventory : MonoBehaviour
                 return;
             }
         }
-        for (int i = 0; i < itemSlots.Length; i++)
+        if(!emptySlotIndex.Equals(empty))
         {
-            if (itemSlots[i].ContainItem == null)
-            {
-                itemSlots[i].AddItem(item);
-                return;
-            }
+            itemSlots[emptySlotIndex].AddItem(item);
+            return;
         }
+        Debug.LogError("인벤토리가 가득 차 있습니다");
     }
 
 
