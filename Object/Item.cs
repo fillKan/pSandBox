57069be49cabfd3bb865312ab10c75773a1cdbe3
한대건 <han15050424@gmail.com>
2020-02-15 17:38:49 +0900

    [슬롯에서 아이템을 넣고 빼는 동작이 자연스러워짐(완성)] #주석 추가 #ItemSlot.cs파일에 SlotSpriteInfo 클래스 추가. #SlotSpriteInfo클래스는 해당 아이템 슬롯에 담긴 아이템을 보여주는 역할을 하는 오브젝트의 정보를 담는다. #담는 정보는, 해당 아이템 슬롯의 sortingLayerID, 스프라이트의 역할을 하는 오브젝트의 각종 컴포넌트를 담는다. #SlotSpriteInfo의 생성자는 인자로 아이템 슬롯의 sortingLayerID를 담는다. #SlotSpriteInfo의 Init()함수는 SortingLayerID를 제외한 정보들을 null로 초기화한다. #SlotSpriteInfo의 생성자와  RegisterAction()함수는 아이템 슬롯의 Awake()함수에서 실행된다. #아이템이 포함하는 함수인 Enter/ExitContainer()함수는 인자로 SlotSpriteInfo를 추가로 받는다. #EnterContainer()함수에서 인자로 받은 SlotSpriteInfo의  SlotSpriteInfo의 자신의 컴포넌트들을 담는다. #ExitContainer()함수에서는 SlotSpriteInfo의 파라미터를 불러와, 자신을 원상복구하는 데에 사용한다.

--- Object/Item.cs
+++ Object/Item.cs
@@ -28,22 +28,23 @@ public abstract class Item : MonoBehaviour
     /// 띄워질 아이템 슬롯의 transform.
     /// </param>
     #endregion
-    public void EnterContainer(Transform parent)
+    public void EnterContainer(Transform parent, SlotSpriteInfo slotSprite)
     {
-        transform.parent = parent;
+        transform.parent        = parent;
         transform.localRotation = Quaternion.Euler(0, 0, 0);
         transform.localPosition = Vector2.zero;
-        transform.localScale = new Vector3(0.7f, 0.7f, 1);
+        transform.localScale    = new Vector3(0.7f, 0.7f, 1);
 
         gameObject.SetActive(true);
-        TryGetComponent<SpriteRenderer>(out SpriteRenderer spr);
-        TryGetComponent<Rigidbody2D>(out Rigidbody2D rigidbody);
-        TryGetComponent<BoxCollider2D>(out BoxCollider2D box);
-        rigidbody.bodyType = RigidbodyType2D.Kinematic;
-        
-        box.enabled = false;
-        spr.sortingLayerID = FindObjectOfType<ItemSlot>().GetComponent<SpriteRenderer>().sortingLayerID;
-        spr.sortingOrder = 1;
+        TryGetComponent<SpriteRenderer> (out slotSprite.Spr);
+        TryGetComponent<Rigidbody2D>    (out slotSprite.Rigidbody);
+        TryGetComponent<BoxCollider2D>  (out slotSprite.Box);
+
+        slotSprite.Rigidbody.bodyType = RigidbodyType2D.Kinematic;
+
+        slotSprite.Box.enabled = false;
+        slotSprite.Spr.sortingLayerID = slotSprite.SortingLayerID;
+        slotSprite.Spr.sortingOrder   = 1;
     }
 
     #region 함수 설명 : 
@@ -51,19 +52,18 @@ public abstract class Item : MonoBehaviour
     /// 해당 아이템이 아이템 슬롯에 띄워지는 스프라이트로써의 역할이 끝났을 때에 실행하는 함수.
     /// </summary>
     #endregion
-    public void ExitContainer()
+    public void ExitContainer(SlotSpriteInfo slotSprite)
     {
-        transform.parent = null;
+        transform.parent     = null;
         transform.localScale = Vector3.one;
 
         gameObject.SetActive(false);
-        TryGetComponent<SpriteRenderer>(out SpriteRenderer spr);
-        TryGetComponent<Rigidbody2D>(out Rigidbody2D rigidbody);
-        TryGetComponent<BoxCollider2D>(out BoxCollider2D box);
-        rigidbody.bodyType = RigidbodyType2D.Dynamic;
+        slotSprite.Rigidbody.bodyType = RigidbodyType2D.Dynamic;
 
-        box.enabled = true;
-        spr.sortingLayerID = ItemMaster.Instance.GetItem(ItemMaster.ItemList.WOOL).GetComponent<SpriteRenderer>().sortingLayerID;
-        spr.sortingOrder = 0;
+        slotSprite.Box.enabled        = true;
+        slotSprite.Spr.sortingLayerID = ItemMaster.Instance.GetItem(ItemMaster.ItemList.WOOL).GetComponent<SpriteRenderer>().sortingLayerID;
+        slotSprite.Spr.sortingOrder   = 0;
+
+        slotSprite.Init();
     }
 }
