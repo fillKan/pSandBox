21e6f3ab45a69554d53d24edd4ee69177ed259e5
한대건 <han15050424@gmail.com>
2020-02-20 17:21:03 +0900

    [아이템의 스프라이트 만을 띄우는 오브젝트의 스크립트 제작] #ItemSlotSprt클래스 추가. #함수 ShowItemSprt()와 HideItemSprt()를 통해 해당 클래스를 컨트롤한다. #함수 ShowItemSprt()는 특정 아이템의 스프라이트를 띄우게 하는 함수이다. #함수 HideItemSprt()는 띄우는 스프라이트를 없애는 함수이다. #아이템슬롯은 해당 클래스를 코드에서 하나 보유하며, 아이템이 들어오고 나갈 때 마다 각각 알맞은 함수를 실행하여 사용한다. #해당 클래스를 컴포넌트로 가지는 오브젝트는 아이템 슬롯의 자식이여야 한다.

---
+++ Object/Item/ItemSlotSprt.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region 클래스 설명 : 
+/// <summary>
+/// 아이템 슬롯에 띄워지는 아이템의 스프라이트를 가지는 오브젝트의 클래스.
+/// <para>
+/// ※ 아이템 슬롯은 이 클래스에 대한 정보를 가지고 있어야 하며, 이 클래스를 가진 오브젝트는 아이템 슬롯의 자식이여야 합니다. ※
+/// </para>
+/// </summary>
+#endregion
+public class ItemSlotSprt : MonoBehaviour
+{
+    public  SpriteRenderer  Renderer
+    {
+        get { return _renderer; }
+    }
+    private SpriteRenderer _renderer;
+
+    private void Start()
+    {
+        // _renderer에 자신의 스프라이트 렌더러를 담는다.
+        if (_renderer == null)
+        {
+            TryGetComponent<SpriteRenderer>(out _renderer);
+        }
+    }
+
+    #region 함수 설명 : 
+    /// <summary>
+    /// 특정 아이템의 스프라이트를 띄우게 하는 함수.
+    /// </summary>
+    /// <param name="itemCode">
+    /// 띄울 아이템 스프라이트의 아이템 코드.
+    /// </param>
+    #endregion
+    public void ShowItemSprt(int itemCode)
+    {
+        _renderer.sprite = ItemMaster.Instance.GetItemSpr(itemCode);
+    }
+
+    #region 함수 설명 : 
+    /// <summary>
+    /// 특정 아이템의 스프라이트를 띄우게 하는 함수.
+    /// </summary>
+    /// <param name="item">
+    /// 띄울 아이템의 ItemMaster.ItemList의 열거자.
+    /// </param>
+    #endregion
+    public void ShowItemSprt(ItemMaster.ItemList item)
+    {
+        _renderer.sprite = ItemMaster.Instance.GetItemSpr(item);
+    }
+
+    #region 함수 설명 : 
+    /// <summary>
+    /// 더 이상 스프라이트 띄우지 마!
+    /// </summary>
+    #endregion
+    public void HideItemSprt()
+    {
+        _renderer.sprite = null;
+    }
+}
