3afb64e4ba3f4a41a222b4ee8ee236028a3228cf
한대건 <han15050424@gmail.com>
2020-02-26 18:26:29 +0900

    [Item.cs에 아이템들의 행동을 담는 인터페이스 선언] #아이템들의 행동을 담는 인터페이스 ItemFunction추가. #인터페이스 ItemFunction는 네가지의 행동을 포함한다. #포함하는 행동은 각각 '아이템을 사용했을 때', '아이템을 들고있을 때', '아이템을 장비하고 있을 때', '아이템이 인벤토리에 존재할 때'이다. #각 행동들은 코루틴으로 표현된다.

--- Object/Item.cs
+++ Object/Item.cs
@@ -2,6 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#region 인터페이스 설명
+/// <summary>
+/// 아이템들의 기능하는 동작 코루틴들을 담는 인터페이스.
+/// <para>※ 기능을 하는 아이템 클래스는 해당 인터페이스를 사용해야함. ※</para> 
+/// </summary>
+#endregion
+public interface ItemFunction
+{
+    #region 코루틴 설명
+    /// <summary>
+    /// 플레이어가 해당 아이템을 들고, 오브젝트와 상호작용 했을 경우의 행동.
+    /// </summary>
+    #endregion
+    public IEnumerator UseItem();
+
+    #region 코루틴 설명
+    /// <summary>
+    /// 플레이어가 해당 아이템을 들고 있을경우의 행동.
+    /// </summary>
+    #endregion
+    public IEnumerator CarryItem();
+
+    #region 코루틴 설명
+    /// <summary>
+    /// 플레이어가 이 아이템을 장비하고 있을때에 행동.
+    /// </summary>
+    #endregion
+    public IEnumerator EquipItem();
+
+    #region 코루틴 설명
+    /// <summary>
+    /// 해당 아이템이 인벤토리에 존재할 경우의 행동.
+    /// </summary>
+    #endregion
+    public IEnumerator InSlotItem();
+}
+
 public abstract class Item : MonoBehaviour
 {
     [HideInInspector]
