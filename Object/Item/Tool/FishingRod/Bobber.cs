61cb916d15c95308c4e749d17be4e8a8d72aed89
한대건 <han15050424@gmail.com>
2020-03-02 09:07:35 +0900

    [낚시찌의 동작 추가] #Lake오브젝트의 태그를 부여. 부여한 태그는 'Water' #낚시찌 오브젝트는 Water태그의 오브젝트와 충돌 했을 때, 5 ~ 10초 가량의 시간을 기다린 후, 0.6초간 작게 진동한다.

--- Object/Item/Tool/FishingRod/Bobber.cs
+++ Object/Item/Tool/FishingRod/Bobber.cs
@@ -6,4 +6,33 @@ public class Bobber : MonoBehaviour
 {
     public Rigidbody2D GetRigidbody2D;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Water"))
+        {
+            StartCoroutine(CR_waitBiting());
+        }
+    }
+
+    private IEnumerator CR_waitBiting()
+    {
+        float waitTime = Random.Range(5.0f, 10.0f);
+
+        for(float i = 0; i < waitTime; i += Time.deltaTime)
+        {
+            yield return null;
+        }
+
+        Vector2 InitPos = transform.position;
+
+        for (float i = 0; i < 0.6f; i += Time.deltaTime)
+        {
+            transform.position = InitPos + (Random.insideUnitCircle * 0.05f);
+
+            yield return null;
+        }
+        transform.position = InitPos;
+
+        yield break;
+    }
 }
