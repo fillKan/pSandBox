f80207351a81126c0372fedebdf8b41cb77a29d1
한대건 <han15050424@gmail.com>
2020-03-03 11:46:59 +0900

    [Lake오브젝트에 파도 효과 추가] #오브젝트의 일렁임(as like 파도)을 보여주는 Wave스크립트 추가. #인스펙터 창에서 입력한 값에따라 파도의 형태가 정해짐. #파도의 형태를 설정하는 값들의 정보는 Tooltip으로 작송됨. #Lake오브젝트의 자식 오브젝트 추가. Lake오브젝트는 더이상 스프라이트 렌더러를 가지지 않고, 자식 오브젝트가 스프라이트 렌더러를 가짐. #결과적으로 '스프라이트를 띄우는 오브젝트'와 '호수의 기능을 하는 오브젝트'로 Lake오브젝트가 분리된 것.
    #Lake메테리얼의 위치를 Ground폴더로 이동.

---
+++ Object/Ground/Wave.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    public enum Tide
+    {
+        FOLLOWING_TIDE,
+        FALL_TIDE
+    }
+
+    [Tooltip("파도의 시작을 밀물과 썰물 중 어느것으로 할 것인지를 설정합니다.")]
+    public Tide StartTide;
+    [Tooltip("파도침을 구현할 오브젝트를 설정합니다.")]
+    public GameObject WavingObjct;
+
+    [Tooltip("파도의 크기의 증감치를 설정합니다.")]
+    public float IncreaseAmount;
+    [Tooltip("파도의 최대 높이를 설정합니다.")]
+    public float MaximumWaveHeight;
+    [Tooltip("파도의 최소 높이를 설정합니다.")]
+    public float MinimumWaveHeight;
+
+    private float sumIncreaseAmount = 0;
+
+    private void Start()
+    {
+        StartCoroutine(CR_update());
+    }
+
+    private IEnumerator CR_update()
+    {
+        while(WavingObjct)
+        {
+            if(StartTide.Equals(Tide.FOLLOWING_TIDE))
+            {
+                if (sumIncreaseAmount <= MaximumWaveHeight)
+                {
+                    sumIncreaseAmount += IncreaseAmount * Time.deltaTime;
+
+                    WavingObjct.transform.Translate(0, IncreaseAmount * Time.deltaTime, 0);
+                }
+                else
+                {
+                    StartTide = Tide.FALL_TIDE;
+                }
+            }
+            else
+            {
+                if (sumIncreaseAmount >= MinimumWaveHeight)
+                {
+                    sumIncreaseAmount -= IncreaseAmount * Time.deltaTime;
+
+                    WavingObjct.transform.Translate(0, -IncreaseAmount * Time.deltaTime, 0);
+                }
+                else
+                {
+                    StartTide = Tide.FOLLOWING_TIDE;
+                }
+            }
+
+            yield return null;
+        }
+
+        yield break;
+    }
+}
