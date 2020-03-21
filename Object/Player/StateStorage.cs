65388ea168ebf575270ca051f5f9337edcdf9ba3
한대건 <han15050424@gmail.com>
2020-03-21 10:31:39 +0900

    [플레이어의 능력치를 저장하는 객체 작업_1 : 능력치를 증감 시키는 기능 추가] #State열거체 추가 : 해당 열거체는 플레이어의 각종 능력들을 열거합니다. #TreeLogging변수 추가 : 해당 변수를 통해 플레이어의 능력치를 반환받을 수 있습니다. #storage딕셔너리 추가 : 해당 딕셔너리는 State열거체와 그에 대치되는 능력치들을 저장합니다. #Awake함수 사용 : 해당 함수를 통해서 storage딕셔너리에 정보들을 담습니다. #IncreaseState함수 추가 : 해당 함수를 통해서 특정 능력치를 원하는만큼 증가시킬 수 있습니다. #DecreaseState함수 추가 : 해당 함수를 통해서 특정 능력치를 원하는만큼 감소시킬 수 있습니다.

--- Object/Player/StateStorage.cs
+++ Object/Player/StateStorage.cs
@@ -2,6 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#region 열거체 설명 :
+/// <summary>
+/// 플레이어의 각종 능력치들을 열거합니다.
+/// </summary>
+#endregion
+public enum States
+{
+    TREE_LOGGING
+}
+
 #region 클래스 설명 :
 /// <summary>
 /// 플레이어의 각종 능력치들의 수치를 저장하는 객체입니다.
@@ -9,5 +19,62 @@ using UnityEngine;
 #endregion
 public class StateStorage : Singleton<StateStorage>
 {
-    
+    #region 변수 설명 :
+    /// <summary>
+    /// 플레이어가 벌목으로 나무의 내구도를 감소시키는 수치입니다.
+    /// </summary>
+    #endregion
+    public  float  TreeLogging
+    {
+        get
+        {
+            return _treeLogging;
+        }
+    }
+    private float _treeLogging;
+
+    private Dictionary<States, float> storage = new Dictionary<States, float>();
+
+    private void Awake()
+    {
+        storage.Add(States.TREE_LOGGING, _treeLogging);
+    }
+
+    #region 함수 설명 :
+    /// <summary>
+    /// 지정한 능력치를 인자값만큼 증가시킵니다.
+    /// </summary>
+    /// <param name="state">
+    /// 증가시킬 능력치를 지정합니다
+    /// </param>
+    /// <param name="figure">
+    /// 증가시킬 수치입니다
+    /// </param>
+    #endregion
+    public void IncreaseState(States state, float figure)
+    {
+        if(storage.ContainsKey(state))
+        {
+            storage[state] += figure;
+        }
+    }
+
+    #region 함수 설명 :
+    /// <summary>
+    /// 지정한 능력치를 인자값만큼 감소시킵니다.
+    /// </summary>
+    /// <param name="state">
+    /// 감소시킬 능력치를 지정합니다
+    /// </param>
+    /// <param name="figure">
+    /// 감소시킬 수치입니다
+    /// </param>
+    #endregion
+    public void DecreaseState(States state, float figure)
+    {
+        if (storage.ContainsKey(state))
+        {
+            storage[state] -= figure;
+        }
+    }
 }
