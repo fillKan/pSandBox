0018e5a475ce148349b66b27dcd3ca30d1e6b7cd
한대건 <han15050424@gmail.com>
2020-03-21 09:52:18 +0900

    [플레이어의 능력치를 저장하는 객체 작업_1 : 아이템에게 기능으로 사용할 수 있는 기능을 직접 제공하기 보다, 능력치를 수정하여 사용하도록 작업] #개요 #아이템이 자신의 기능으로서 사용할 수 있는 기능을 직접 제공하기 보다, 능력치의 수치를 수정하여 사용하도록 수정.
    #플레이어의 각종 능력치들의 수치를 저장하는 클래스, StateStorage추가. StateStorage는 싱글턴 객체입니다.

---
+++ Object/Player/StateStorage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region 클래스 설명 :
+/// <summary>
+/// 플레이어의 각종 능력치들의 수치를 저장하는 객체입니다.
+/// </summary>
+#endregion
+public class StateStorage : Singleton<StateStorage>
+{
+    
+}
