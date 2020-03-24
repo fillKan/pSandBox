4985104f13315ec47c2b9aa6e716d5496bc1c40e
한대건 <han15050424@gmail.com>
2020-03-24 10:31:26 +0900

    [키보드 키를 통해 가장 가까이에 있는 아이템을 줍는 기능_1 : 범위내의 아이템을 감지하는 레이더 추가] #ItemRadar는 트리거로 설정된 서클 콜라이더와 같이 사용되어, 콜라이더와 접촉한 아이템들을 자신의 senseItem딕셔너리에 저장합니다. #그리고 ItemRadar의 함수 GetCloseItem함수를 통해, 서클 콜라이더의 중심과 가장 가까운 아이템을 찾을 수 있습니다.

---
+++ Object/Item/ItemRadar.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRadar : MonoBehaviour
+{
+    private Dictionary<int, ItemExisting> senseItem = new Dictionary<int, ItemExisting>();
+
+    #region 함수 설명 :
+    /// <summary>
+    /// 해당 레이더와 가장 가까이 있는 아이템을 반환합니다.
+    /// </summary>
+    #endregion
+    public ItemExisting GetCloseItem()
+    {
+        ItemExisting closestItem = null;
+
+        float distance = 0; float closestDistance = 0;
+
+        foreach(KeyValuePair<int,ItemExisting> item in senseItem)
+        {
+            distance = Vector2.Distance(transform.position, item.Value.transform.position);
+
+            if(distance < closestDistance)
+            {
+                closestDistance = distance;
+
+                closestItem = item.Value;
+            }
+        }
+        return closestItem;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent(out ItemExisting item))
+        {
+            if(senseItem.ContainsKey(item.gameObject.GetInstanceID()))
+            {
+                senseItem.Add(item.gameObject.GetInstanceID(), item);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(senseItem.ContainsKey(collision.gameObject.GetInstanceID()))
+        {
+            senseItem.Remove(collision.gameObject.GetInstanceID());
+        }
+    }
+}
