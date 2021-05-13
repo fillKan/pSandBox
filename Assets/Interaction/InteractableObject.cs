a938529f32c8fa7e4db38b6ce97a08c88dac77f2
kimbokChi <han15050424@gmail.com>
2021-05-13 10:15:47 +0900

    IInteraction인터페이스를 대체할 클래스를 추가한다
    추가한 클래스의 이름은 "InteractableObject"로, 상호작용하는 행동과 하이라이트 했을 때의 행동을 구현한다. 또한 스프라이트 렌더러를 프로퍼티로 포함한다.

---
+++ Assets/Interaction/InteractableObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InteractableObject : MonoBehaviour
+{
+    protected static readonly Color _HighLightColor = new Color(0.9f, 1.0f, 0.5f);
+
+    [Header("Intractable Property")]
+    [SerializeField]
+    protected SpriteRenderer _Renderer;
+    public SpriteRenderer Renderer
+    { get => _Renderer; }
+
+    public abstract void Interaction();
+    public virtual void OnHighLight()
+    {
+        _Renderer.color = _HighLightColor;
+    }
+}
