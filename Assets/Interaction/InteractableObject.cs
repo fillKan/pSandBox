1a11b0b65f7a6c37a0ffc43bd53d4bba55ea5b0e
kimbokChi <han15050424@gmail.com>
2021-05-13 13:13:17 +0900

    InteractableObject의 하이라이트가 해제되었을 때의 행동을 추가한다
    이 행동은 가상함수로, DisHighLight함수를 통해 구현된다. InteractableObject에서 정의된 행동은
    렌더러의 색을 기본 색으로 변경하는 것이다. 이 '기본 색'은 인스펙터에서 바꿀 수 있다.

--- Assets/Interaction/InteractableObject.cs
+++ Assets/Interaction/InteractableObject.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public abstract class InteractableObject : MonoBehaviour
 {
     protected static readonly Color _HighLightColor = new Color(0.9f, 1.0f, 0.5f);
-
     [Header("Intractable Property")]
-    [SerializeField]
-    protected SpriteRenderer _Renderer;
+    [SerializeField] protected SpriteRenderer _Renderer;
+    [SerializeField] protected Color _DefaultColor = Color.white;
+
     public SpriteRenderer Renderer
     { get => _Renderer; }
 
@@ -16,6 +16,10 @@ public abstract class InteractableObject : MonoBehaviour
     public virtual void OnHighLight()
     {
         _Renderer.color = _HighLightColor;
+    }
+    public virtual void DisHighLight()
+    {
+        _Renderer.color = _DefaultColor;
     }
     public virtual void OnActive() { }
     private void OnEnable()
