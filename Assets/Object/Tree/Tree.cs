400f257f4027f550ddcb8a3142cccc1d150c762e
kimbokChi <han15050424@gmail.com>
2021-05-16 21:09:03 +0900

    Tree클래스는 추상적이지 않으며 모든 나무는 Tree클래스를 통해 구현된다
    참나무를 구현하는 클래스와 자작나무를 구현하는 두 클래스의 행동은 별 차이가 없기 때문에,
    이 둘을 하나의 클래스로 구현한다.
    
    이에 따라 기존의 참나무 클래스와 자작나무 클래스를 제거했고,
    각 나무의 프리팹에도 이 변경사항이 적용되었다.

--- Assets/Object/Tree/Tree.cs
+++ Assets/Object/Tree/Tree.cs
@@ -2,125 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Tree : MonoBehaviour
+public class Tree : InteractableObject
 {
-    #region 변수 설명 : 
-    /// <summary>
-    /// 해당 나무의 SpriteRenderer를 불러옵니다.
-    /// </summary>
-    #endregion
-    public    SpriteRenderer  SprtRenderer
+    [Header("Tree Property")]
+    [SerializeField] private float _Durability;
+    [SerializeField] private ItemDropper _ItemDropper;
+    [SerializeField] private Collider2D _Collider;
+
+    [SerializeField] private Animator _Animator;
+    private int _AnimControlKey;
+
+    [Header("Shaking Property")]
+    [SerializeField] private AnimationCurve _ShakingCurve;
+
+    private float _RestShakeTime = 0f;
+    private float _ShakeTime = 0f;
+    private float _ShakeForcePerFrame = 0f;
+
+    private float _RestDurability;
+
+    public override void OnActive()
     {
-        get { return _sprtRenderer; }
+        base.OnActive();
+
+        StartCoroutine(ShakingRoutine());
+        _AnimControlKey = _Animator.GetParameter(0).nameHash;
+
+        _RestDurability = _Durability;
+        _Collider.enabled = true;
     }
-    protected SpriteRenderer _sprtRenderer;
-
-    #region 변수 설명 : 
-    /// <summary>
-    /// 해당 나무를 베고 있는지의 여부를 불러옵니다.
-    /// </summary>
-    #endregion
-    public    bool  DoingChopTree
+    public override void Interaction()
     {
-        get { return _doingChopTree; }
-    }
-    protected bool _doingChopTree = false;
+        Shaking(0.3f, 0.05f);
 
-    #region 변수 설명 : 
-    /// <summary>
-    /// 해당 나무가 쓰러지고 있는지의 여부를 불러옵니다.
-    /// </summary>
-    #endregion
-    public    bool  IsCutDown
-    {
-        get { return _isCutDown; }
-    }
-    protected bool _isCutDown = false;
-
-    #region 변수 설명 : 
-    /// <summary>
-    /// 해당 나무의 내구도를 불러옵니다.
-    /// </summary>
-    #endregion
-    public    float  fDurability
-    {
-        get { return _fDurability; }
-    }
-    protected float _fDurability;
-
-    private void Start()
-    {
-        InitTree();
-    }
-
-    #region 코루틴 설명 : 
-    /// <summary>
-    /// 나무를 뱁니다.
-    /// </summary>
-    /// <param name="cutAmount">
-    /// 해당 값만큼 나무의 내구도가 줄어듦니다.
-    /// </param>
-    /// <param name="vibTime">
-    /// 해당 값만큼 나무를 베는데에 딜레이가 걸리며, 나무가 흔들립니다.
-    /// </param>
-    /// <param name="vibAmount">
-    /// 나무가 흔들리는 정도를 나타냅니다.
-    /// </param>
-    /// <returns></returns>
-    #endregion
-    public IEnumerator CR_chopTree(float cutAmount, float vibTime, float vibAmount)
-    {
-        _doingChopTree = true;
-        _fDurability  -= cutAmount;
-
-        Vector2 vInitPos = transform.position;
-        
-        while (vibTime > 0)
+        if ((_RestDurability -= StateStorage.Instance.TreeLogging) <= 0)
         {
-            vibTime -= Time.deltaTime;
-            
-            transform.position = ((Vector2)Random.insideUnitSphere * vibAmount) + vInitPos;
+            _ItemDropper.DropItem(0);
+
+            InteractionManager.Instance.DisRegister(gameObject.GetInstanceID());
+            _Collider.enabled = false;
+            _Animator.SetInteger(_AnimControlKey, 1);
+
+            CursorPointer.Instance.HighLightRelease();
+        }
+    }
+    private void Shaking(float time, float force)
+    {
+        float forcePerFrame = force / time;
+        float ratio = 1f - Mathf.Min(_RestShakeTime / _ShakeTime, 1f);
+
+        if (forcePerFrame > _ShakeForcePerFrame * _ShakingCurve.Evaluate(ratio))
+        {
+            _ShakeForcePerFrame = forcePerFrame;
+            _RestShakeTime = _ShakeTime = time;
+        }
+        else
+        {
+            _ShakeForcePerFrame += forcePerFrame;
+        }
+    }
+    private IEnumerator ShakingRoutine()
+    {
+        while (gameObject.activeInHierarchy) 
+        {
+            if (_RestShakeTime > 0)
+            {
+                _RestShakeTime -= Time.deltaTime;
+
+                float ratio = 1f - _RestShakeTime / _ShakeTime;
+                
+                _Renderer.transform.localPosition 
+                    = Random.insideUnitCircle * _ShakeForcePerFrame * _ShakingCurve.Evaluate(ratio);
+
+                if (_RestShakeTime <= 0f) {
+                    _RestShakeTime = _ShakeForcePerFrame = _ShakeTime = 0f;
+                }
+            }
             yield return null;
         }
-        transform.position = vInitPos;
-        _doingChopTree     = false;
-        yield break;
     }
 
-    #region 코루틴 설명 : 
-    /// <summary>
-    /// 나무가 서서히 사라지며, 끝내 비활성화 됩니다.
-    /// </summary>
-    #endregion
-    public IEnumerator CR_cutDown()
+    private void AE_FadeCompleted()
     {
-        _isCutDown  = true;
-        float alpha = 1;
-
-        while (SprtRenderer.color.a > 0)
-        {
-            alpha -= 0.02f;
-            SprtRenderer.color = new Color(1, 1, 1, alpha);
-
-            yield return null;
-        }
         gameObject.SetActive(false);
-        _isCutDown = false;
-
-        yield break;
     }
-
-    #region 설명 :
-    /// <summary>
-    /// 나무를 베고나서 드롭되는 아이템을 담는다.
-    /// </summary>
-    #endregion
-    public abstract void DropItem();
-
-    #region 설명 :
-    /// <summary>
-    /// 오브젝트가 활성화 될 때 실행된다.(초기화)
-    /// </summary>
-    #endregion
-    protected abstract void InitTree();
 }
