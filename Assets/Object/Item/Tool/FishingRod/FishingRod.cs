22e3b30f6e0d8cb8a2d967938e4915776067f1f3
kimbokChi <han15050424@gmail.com>
2021-05-19 15:02:23 +0900

    낚시대 스크립트를 수정한다
    현재 브랜치의 개발 목적은 낚시대 하나를 두 개의 아이템으로 구현하지 않고, 하나의 아이템으로
    구현하는 것이 목적이다.
    
    현재 낚시대 아이템으로 낚시찌를 던지고, 다시 회수하는(힘을 가해서 회수하지는 않는다)동작을
    구현했다. 이를 구현하기 위해 낚시대 프리팹에게 낚시줄, 낚시찌, 낚시대의 끝지점을 나타내는
    오브젝트를 추가했다.

--- Assets/Object/Item/Tool/FishingRod/FishingRod.cs
+++ Assets/Object/Item/Tool/FishingRod/FishingRod.cs
@@ -2,51 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FishingRod : Item, IItemFunction
+public class FishingRod : Item, IEquipItem
 {
-    public IEnumerator CarryItem(ItemSlot itemSlot)
+    [Header("FishingRod Property")]
+    [SerializeField] private Sprite _UsedSprite;
+    [SerializeField] private Sprite _DefaultSprite;
+    [SerializeField] private SpriteRenderer _Renderer;
+
+    [SerializeField] private LineRenderer _LineRenderer;
+    [SerializeField] private Transform _RodTopPoint;
+
+    [Header("Bobber Property")]
+    [SerializeField] private SecondryCollider _Bobber;
+    [SerializeField] private Rigidbody2D _BobberRigid;
+    [SerializeField] private float _ThrowingForce;
+
+    private CursorPointer _Cursor;
+
+    private bool _IsEquiped;
+    private bool _IsUsed = false;
+
+    public void OnEquipItem()
     {
-        if (MouseCursor.Instance.RightClickVoid)
+        _IsEquiped = true;
+        StartCoroutine(UpdateRoutine());
+
+        _Bobber.gameObject.SetActive(false);
+        _LineRenderer.gameObject.SetActive(false);
+
+        _Cursor ??= CursorPointer.Instance;
+    }
+    public void DisEquipItem()
+    {
+        _IsEquiped = false;
+        DisThrowBobber();
+    }
+    private IEnumerator UpdateRoutine()
+    {
+        while (_IsEquiped)
         {
-            // itemSlot.SetItem(ItemName.FISHING_ROD_USED);
+            if (Input.GetMouseButtonDown(1))
+            {
+                if (_Cursor.IsOnVoid())
+                {
+                    if (_IsUsed)
+                    {
+                        DisThrowBobber();
+                    }
+                    else
+                    {
+                        OnThrowBobber();
+                    }
+                    _IsUsed = !_IsUsed;
+                }
+            }
+            if (_IsUsed)
+            {
+                _LineRenderer.SetPosition(0, _RodTopPoint.position);
+                _LineRenderer.SetPosition(1, _Bobber.transform.position);
+            }
+            yield return null;
         }
-        yield break;
     }
-
-    public IEnumerator MountItem()
+    private void OnThrowBobber()
     {
-        yield break;
+        _Renderer.sprite = _UsedSprite;
+
+        _Bobber.transform.SetParent(null);
+        _Bobber.gameObject.SetActive(true);
+        _LineRenderer.gameObject.SetActive(true);
+
+        Vector2 dir = (_Cursor.transform.position - _RodTopPoint.position);
+        _BobberRigid.AddForce(dir.normalized * _ThrowingForce * dir.magnitude);
     }
-
-    public IEnumerator InSlotItem()
+    private void DisThrowBobber()
     {
-        yield break;
+        _Renderer.sprite = _DefaultSprite;
+
+        _Bobber.transform.SetParent(transform);
+        _Bobber.gameObject.SetActive(false);
+        _LineRenderer.gameObject.SetActive(false);
+
+        _Bobber.transform.position = _RodTopPoint.position;
+        _BobberRigid.velocity = Vector2.zero;
     }
-
-    public IEnumerator UseItem<T>(T xValue) where T : IInteraction
+    public override bool IsUsing(ItemInterface itemInterface)
     {
-        yield break;
-    }
-
-    private void Awake()
-    {
-        _itemCode = (int)ItemName.FISHING_ROD;
-
-        _itemType = ItemTypeList.TOOL;
-    }
-
-    public IEnumerator UnmountItem()
-    {
-        yield break;
-    }
-
-    public bool HasFunction(ItemInterface func)
-    {
-        switch (func)
-        {
-            case ItemInterface.Equip:
-                return true;
-        }
-        return false;
+        return itemInterface == ItemInterface.Equip;
     }
 }
